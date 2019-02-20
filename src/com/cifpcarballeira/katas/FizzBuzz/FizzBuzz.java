ca90c9f30c37ec02cd1ff3c07221295dd59a92f5
Ismael Lage <mrlage99@gmail.com>
2019-02-20 10:29:41 +0100

    1 devuelve 1

---
+++ src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cifpcarballeira.katas.FizzBuzz;
+
+/**
+ *
+ * @author dam112
+ */
+public class FizzBuzz {
+
+    public String getFizzBuzz(int num) {
+
+        return "1";
+    }
+
+}
